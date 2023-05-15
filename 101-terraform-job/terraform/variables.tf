6d4f21bd943f08b11a67778dbece3c43fca5143d
qiangniuairbus <61810122+qiangniuairbus@users.noreply.github.com>
2023-05-15 10:05:30 +0800

    Update variables.tf with Region

--- 101-terraform-job/terraform/variables.tf
+++ 101-terraform-job/terraform/variables.tf
@@ -13,7 +13,7 @@ variable "environment" {
 variable "location" {
   type    = string
   description = "Azure region where to create resources."
-  default = "North Europe"
+  default = "China North 3"
 }
 
 variable "department" {
